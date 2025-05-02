6457746272b8355d90ff52159bc9c6d4ad9c7ddd
luckas-lopes <luckas.silva@sptech.school>
2025-05-02 16:11:25 -0300

    Verificação de credênciais de cadastro adicionada

---
+++ js/cadastro.js
@@ -0,0 +1,141 @@
+function redirecionarLogin() {
+    window.location = 'login.html';
+}
+
+function validarNomeCorreto() {
+
+    var nome = iptNome.value;
+    legendaNome.innerHTML = '';
+
+    if (nome.length >= 1) {
+        iptNome.style.borderColor = '#36b0c9'
+    }
+
+}
+
+function validarNomeErrado() {
+
+    var nome = iptNome.value;
+
+    if (nome.length < 1) {
+        iptNome.style.borderColor = '#ff0000'
+        legendaNome.innerHTML = 'Esse campo é obrigatório!'
+    }
+}
+
+function validarEmailCorreto() {
+
+    var email = iptEmail.value;
+    legendaEmail.innerHTML = ''
+
+    if (email.includes('@') && email.includes('.')) {
+        iptEmail.style.borderColor = '#36b0c9';
+    }
+}
+
+function validarEmailErrado() {
+
+    var email = iptEmail.value;
+
+    if ((email.includes('@') != true) || (email.includes('.') != true)) {
+        legendaEmail.innerHTML = 'Insira um endereço de email válido!'
+        iptEmail.style.borderColor = '#ff0000';
+    }
+
+    if (email.length < 1) {
+        iptEmail.style.borderColor = '#ff0000'
+        legendaEmail.innerHTML = 'Esse campo é obrigatório!'
+    }
+}
+
+function validarSenhaCorreto() {
+    var senha = iptSenha.value;
+    var especial = ',.;:?!...+-*/=%<>±÷×≠≈≤≥()[]{}<>&|^~!"`@#$_:;°\§©®™¶' + "'";
+
+    var caractereEspecial = false;
+    var minCaractere = false;
+    var mai = false;
+    var min = false;
+
+    legendaSenha.innerHTML = '';
+
+    // Caractere Especial
+    for (var i = 0; i < senha.length; i++) {
+
+        var caractereAtual = senha[i];
+        for (var j = 0; j < especial.length; j++) {
+            if (caractereAtual == especial[j]) {
+                caractereEspecial = true;
+            }
+        }
+
+    }
+
+    if (caractereEspecial == false) {
+    } else {
+
+    }
+
+    // Mínimo de 8 Caracteres
+    if (senha.length >= 8) {
+        minCaractere = true;
+    }
+
+    // Verificar Maiúscula e Minúscula
+    if (senha.toUpperCase() != senha) {
+        min = true;
+    }
+    if (senha.toLowerCase() != senha) {
+        mai = true;
+    }
+
+    // Trocar cor da borda
+    if ((min == true) && (mai == true) && (caractereEspecial == true) && (minCaractere == true)) {
+        iptSenha.style.borderColor = "#36b0c9";
+    }
+
+    return [min, mai, caractereEspecial, minCaractere];
+}
+
+function validarSenhaErrado() {
+    var incorreta = validarSenhaCorreto();
+    if ((incorreta[0] != true) || (incorreta[1] != true) || (incorreta[2] != true) || (incorreta[3] != true)) {
+        iptSenha.style.borderColor = "#ff0000"
+    }
+
+    if (incorreta[0] != true) {
+        legendaSenha.innerHTML = 'Crie uma senha com pelo menos uma letra minúscula';
+    }
+    if (incorreta[1] != true) {
+        legendaSenha.innerHTML = 'Crie uma senha com pelo menos uma letra maiúscula';
+    }
+    if (incorreta[2] != true) {
+        legendaSenha.innerHTML = 'Crie uma senha com pelo menos um caractere especial. ex(. , / $)';
+    }
+    if (incorreta[3] != true) {
+        legendaSenha.innerHTML = 'Crie uma senha com pelo menos 8 caracteres';
+    }
+
+}
+
+function validarConfirmarSenhaCorreto() {
+
+    var confirmar = iptConfirmarSenha.value;
+    var senha = iptSenha.value;
+    legendaConfirmarSenha.innerHTML = '';
+
+    if (senha == confirmar) {
+        iptConfirmarSenha.style.borderColor = "#36b0c9"
+    }
+}
+
+function validarConfirmarSenhaErrado() {
+
+    var confirmar = iptConfirmarSenha.value;
+    var senha = iptSenha.value;
+
+    if ((senha != confirmar) || (confirmar.length < 1)) {
+        iptConfirmarSenha.style.borderColor = "#ff0000"
+        legendaConfirmarSenha.innerHTML = 'As senhas não conferem!';
+    }
+}
