303784250076f2d73d50b8f5ea335aeac53bf19c
luckas-lopes <luckas.silva@sptech.school>
2025-05-10 21:58:01 -0300

    Mecânicas e estilização atualizadas e correções de bugs feito

--- public/js/minigame.js
+++ public/js/minigame.js
@@ -1,6 +1,6 @@
-var botoes4x4 = ['botao1', 'botao2', 'botao3', 'botao4', 'botao5', 'botao6', 'botao7', 'botao8', 'botao9', 'botao10', 'botao11', 'botao12', 'botao13', 'botao14', 'botao15', 'botao16'];
-var botoes5x5 = ['botao1', 'botao2', 'botao3', 'botao4', 'botao5', 'botao6', 'botao7', 'botao8', 'botao9', 'botao10', 'botao11', 'botao12', 'botao13', 'botao14', 'botao15', 'botao16', '17', 'botao18', 'boato19', 'botao20', 'botao21', '22', 'botao23', 'botao24', 'botao25'];
-var botoes2x2 = ['botao1', 'botao2', 'botao3', 'botao4']
+var botoes4x4 = ['botao1-4x4', 'botao2-4x4', 'botao3-4x4', 'botao4-4x4', 'botao5-4x4', 'botao6-4x4', 'botao7-4x4', 'botao8-4x4', 'botao9-4x4', 'botao10-4x4', 'botao11-4x4', 'botao12-4x4', 'botao13-4x4', 'botao14-4x4', 'botao15-4x4', 'botao16-4x4'];
+var botoes5x5 = ['botao1-5x5', 'botao2-5x5', 'botao3-5x5', 'botao4-5x5', 'botao5-5x5', 'botao6-5x5', 'botao7-5x5', 'botao8-5x5', 'botao9-5x5', 'botao10-5x5', 'botao11-5x5', 'botao12-5x5', 'botao13-5x5', 'botao14-5x5', 'botao15-5x5', 'botao16-5x5', '17-5x5', 'botao18-5x5', 'boato19-5x5', 'botao20-5x5', 'botao21-5x5', '22-5x5', 'botao23-5x5', 'botao24-5x5', 'botao25-5x5'];
+var botoes2x2 = ['botao1-2x2', 'botao2-2x2', 'botao3-2x2', 'botao4-2x2']
 
 var opcao1 = 0;
 var opcao2 = 0;
@@ -18,6 +18,7 @@ function comecar() {
     intervaloFinalizar = setTimeout(finalizar, segundos * 1000);
     intervaloContador = setInterval(contador, 1000);
 
+    document.getElementById('minigame').innerHTML = '';
     document.getElementById('contador').innerHTML += '<div id="divContador"></div>';
     document.getElementById('botaoComecar').disabled = true;
 
@@ -29,6 +30,7 @@ function atualizar() {
 
     pontuacao += 300;
 
+    // Remove todas as imagens anteriores
     if (numBotoes == 0) {
         for (var i = 0; i < botoes4x4.length; i++) {
             document.getElementById(botoes4x4[i]).remove();
@@ -43,16 +45,20 @@ function atualizar() {
         }
     }
 
-    var imgAleatoria = Math.round(Math.random())
-
+    // Escolhe de forma aleatória entre as duplas de imagens
+    var imgAleatoria = Math.round(Math.random() * 2);
     if (imgAleatoria == 0) {
-        img1 = '<img draggable="false" src="./img/bobOoO.png" width="80%">';
-        img2 = '<img draggable="false" src="./img/patrickOoO.png" width="70%">';
-    } else {
+        img1 = '<img draggable="false" src="./img/bobOoO.png" width="90%">';
+        img2 = '<img draggable="false" src="./img/patrickOoO.png" width="90%">';
+    } else if(imgAleatoria == 1) {
         img1 = '<img draggable="false" src="./img/bobCaverna.png" width="100%">';
         img2 = '<img draggable="false" src="./img/patrickCaverna.png" width="100%">';
-    }
+    } else if(imgAleatoria == 2) {
+        img1 = '<img draggable="false" src="./img/bobPintura.jpeg" width="100%" height="100%">';
+        img2 = '<img draggable="false" src="./img/patrickPintura.jpeg" width="100%" height="100%">';
+    } 
 
+    // Cria os botões em sequência e quantidade aleatórias
     numBotoes = Math.round(Math.random() * 2);
     if (numBotoes == 0) {
 
@@ -70,7 +76,6 @@ function atualizar() {
 
             }
         }
-
     } else if (numBotoes == 1) {
 
         for (var i = 0; i < botoes2x2.length; i++) {
@@ -79,6 +84,17 @@ function atualizar() {
             var num = Math.round(Math.random());
 
             document.getElementById('minigame').innerHTML += `<div onclick="clicou(${i + 1})" id="${botaoAtual}" class="botao2x2"></div>`;
+
+            if (botaoAtual == 0) {
+                document.getElementById(botaoAtual).style.borderTopLeftRadius = '30px';
+            } else if (botaoAtual == 1) {
+                document.getElementById(botaoAtual).style.borderTopRightRadius = '30px';
+            } else if (botaoAtual == 2) {
+                document.getElementById(botaoAtual).style.borderBottomLeftRadius = '30px';
+            } else if (botaoAtual == 3) {
+                document.getElementById(botaoAtual).style.borderBottomRightRadius = '30px';
+            }
+
             if (num == 0) {
                 document.getElementById(botaoAtual).innerHTML = img1
 
@@ -87,7 +103,6 @@ function atualizar() {
 
             }
         }
-
     } else if (numBotoes == 2) {
 
         for (var i = 0; i < botoes5x5.length; i++) {
@@ -104,9 +119,9 @@ function atualizar() {
 
             }
         }
-
     }
 
+    // Recria as imagens caso já venham certas
     if (numBotoes == 0) {
 
         for (var i = 0; i < botoes4x4.length; i++) {
@@ -116,9 +131,8 @@ function atualizar() {
             } else if (document.getElementById(botoes4x4[i]).innerHTML == img2) {
                 opcao2++;
             }
-
         }
-
+        
         if ((opcao1 == 16) || (opcao2 == 16)) {
 
             atualizar();
@@ -137,9 +151,9 @@ function atualizar() {
         }
 
         if ((opcao1 == 4) || (opcao2 == 4)) {
-
             atualizar();
         }
+
     } else if (numBotoes == 2) {
 
         for (var i = 0; i < botoes5x5.length; i++) {
@@ -153,13 +167,9 @@ function atualizar() {
         }
 
         if ((opcao1 == 25) || (opcao2 == 25)) {
-
             atualizar();
         }
     }
-
-
-
 
     atualizarContador();
 }
@@ -169,8 +179,10 @@ function clicou(botao) {
     opcao1 = 0;
     opcao2 = 0;
 
+    
     if (numBotoes == 0) {
 
+        // Troca a imagem do botão para sua contraparte
         if (document.getElementById(botoes4x4[botao - 1]).innerHTML == img1) {
             document.getElementById(botoes4x4[botao - 1]).innerHTML = img2
 
@@ -179,8 +191,8 @@ function clicou(botao) {
 
         }
 
+        // Confirma se todas imagens estão iguais
         for (var i = 0; i < botoes4x4.length; i++) {
-
             if (document.getElementById(botoes4x4[i]).innerHTML == img1) {
                 opcao1++;
             } else if (document.getElementById(botoes4x4[i]).innerHTML == img2) {
@@ -189,12 +201,13 @@ function clicou(botao) {
 
         }
 
+        // Caso estejam iguais, aumenta a contagem com base na dificuldade que acabou de passar na tela
         if ((opcao1 == 16) || (opcao2 == 16)) {
 
             clearTimeout(intervaloFinalizar);
             if (segundos < 15) {
-                intervaloFinalizar = setTimeout(finalizar, segundos * 1000 + 1000);
-                segundos += 1;
+                intervaloFinalizar = setTimeout(finalizar, segundos * 1000 + 1500);
+                segundos += 1.5;
             }
 
             atualizar();
@@ -224,8 +237,8 @@ function clicou(botao) {
 
             clearTimeout(intervaloFinalizar)
             if (segundos < 15) {
-                intervaloFinalizar = setTimeout(finalizar, segundos * 1000 + 500);
-                segundos += 0.5;
+                intervaloFinalizar = setTimeout(finalizar, segundos * 1000 + 1000);
+                segundos += 1;
             }
 
             atualizar();
@@ -258,7 +271,6 @@ function clicou(botao) {
                 intervaloFinalizar = setTimeout(finalizar, segundos * 1000 + 2000);
                 segundos += 2;
             }
-
             atualizar();
         }
 
@@ -268,6 +280,7 @@ function clicou(botao) {
 
 function finalizar() {
 
+    // Quando o tempo chega ao fim, exclui todos os elementos
     if (numBotoes == 0) {
         for (var i = 0; i < botoes4x4.length; i++) {
             document.getElementById(botoes4x4[i]).remove();
@@ -283,7 +296,6 @@ function finalizar() {
     }
 
     document.getElementById('divContador').remove();
-    document.getElementById('botaoJogarNovamente').disabled = false;
 
     clearInterval(intervaloContador)
 
@@ -314,7 +326,7 @@ function atualizarContador() {
     segundosContador = [];
     document.getElementById('divContador').innerHTML = '';
 
-
+    // Cria a interface de contador
     for (var i = 0; i < Math.trunc(segundos); i++) {
 
         segundosContador.push(`<div id="segundosContador${i}" class="segundosContador"></div>`)
